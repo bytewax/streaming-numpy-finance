53764fae272f254afdf8cdb7610db9ef7190c125
awmatheson <a.w.matheson@gmail.com>
2023-02-28 09:46:52 -0800

    update example

--- dataflow.py
+++ dataflow.py
@@ -15,19 +15,19 @@ from websocket import create_connection
 from ticker_pb2 import Ticker
 
 ## input
-ticker_list = ['BTC-USD']
+ticker_list = ['AMZN', 'MSFT']
 
 def yf_input(worker_tickers, state):
-    while True:
-        for asset in worker_tickers:
-            ws = create_connection("wss://streamer.finance.yahoo.com/")
-            ws.send(json.dumps({"subscribe": worker_tickers}))
+        ws = create_connection("wss://streamer.finance.yahoo.com/")
+        ws.send(json.dumps({"subscribe": worker_tickers}))
+        while True:
             yield state, ws.recv()
 
 
 def input_builder(worker_index, worker_count, resume_state):
     state = resume_state or None
-    worker_tickers = distribute(ticker_list, worker_index, worker_count)
+    worker_tickers = list(distribute(ticker_list, worker_index, worker_count))
+    print({"subscribing to": worker_tickers})
     return yf_input(worker_tickers, state)
 
 
@@ -49,16 +49,15 @@ def deserialize(message):
     
 
 flow.map(deserialize)
-flow.inspect(print)
 
 
 def build_array():
-    return np.empty((0,2))
+    return np.empty((0,3))
 
 
 # This is the accumulator function, and outputs a numpy array of time and price
 def acc_values(np_array, ticker):
-    return np.insert(np_array, 0, np.array((ticker.time, ticker.price)), 0)
+    return np.insert(np_array, 0, np.array((ticker.time, ticker.price, ticker.dayVolume)), 0)
 
 
 # This function instructs the event clock on how to retrieve the
@@ -79,10 +78,11 @@ start_at = start_at - timedelta(
 )
 wc = TumblingWindowConfig(start_at=start_at, length=timedelta(seconds=60))
 flow.fold_window("1_min", cc, wc, build_array, acc_values)
-# technical analysis
+flow.inspect(print)
 
-def calculate_features(data):
-    return({"time":data[0][0], "min":np.amin(data[:,1]), "max":np.amax(data[:,1]), "first_price":data[:,1][0], "last_price":data[:,1][-1]})
+def calculate_features(ticker__data):
+    ticker, data = ticker__data
+    return (ticker, {"time":data[-1][0], "min":np.amin(data[:,1]), "max":np.amax(data[:,1]), "first_price":data[:,1][-1], "last_price":data[:,1][0], "volume":data[:,2][0] - data[:,2][-1]})
 
 flow.map(calculate_features)
 
